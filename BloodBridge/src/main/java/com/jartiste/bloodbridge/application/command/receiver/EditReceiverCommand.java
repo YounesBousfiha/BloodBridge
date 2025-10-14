936b2b2a5a4aef3afde138fd7d6f2665046283b7
Coderyounes <younesanas2021@gmail.com>
2025-10-14 20:47:16 +0100

    FEAT: Update EditReceiverCommand to improve receiver retrieval and update logic

--- BloodBridge/src/main/java/com/jartiste/bloodbridge/application/command/receiver/EditReceiverCommand.java
+++ BloodBridge/src/main/java/com/jartiste/bloodbridge/application/command/receiver/EditReceiverCommand.java
@@ -3,6 +3,7 @@ package com.jartiste.bloodbridge.application.command.receiver;
 import com.jartiste.bloodbridge.application.command.Command;
 import com.jartiste.bloodbridge.application.command.CommandResult;
 import com.jartiste.bloodbridge.application.service.ReceiverService;
+import com.jartiste.bloodbridge.domain.enums.BloodType;
 import com.jartiste.bloodbridge.presentation.dto.ReceiverDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,45 +14,33 @@ import java.io.IOException;
 public class EditReceiverCommand implements Command {
 
     public static final String ERROR = "error";
-    public static final String PATH = "/donors";
+    public static final String PATH = "/receivers/list";
 
     @Override
     public CommandResult execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ReceiverService receiverService = getService(request, ReceiverService.class);
 
-        if("GET".equalsIgnoreCase(request.getMethod())) {
-            String receiverID = request.getParameter("receiverId");
-            if(null == receiverID || receiverID.isEmpty()) {
-                request.getSession().setAttribute(ERROR, "Receiver ID is required");
-                return CommandResult.redirect(request.getContextPath() + PATH);
-            }
-
-            try {
-                var receiver = receiverService.getReceiverById(Long.parseLong(receiverID));
-                if(receiver != null) {
-                    request.setAttribute("receiver", receiver);
-                    return CommandResult.forward("/WEB-INF/views/receiver/editReceiverForm.jsp");
-                } else {
-                    request.getSession().setAttribute(ERROR, "Receiver not found");
-                    return CommandResult.redirect(request.getContextPath() + PATH);
-                }
-            } catch (Exception e) {
-                request.getSession().setAttribute(ERROR, "An error occur while fetching the receiver");
-                return CommandResult.redirect(request.getContextPath() + PATH);
-            }
-        }
-
         if("POST".equalsIgnoreCase(request.getMethod())) {
-            ReceiverDTO dto = new ReceiverDTO();
-            dto.setId(Long.parseLong(request.getParameter("id")));
-            dto.setFirstName(request.getParameter("firstName"));
-            dto.setLastName(request.getParameter("lastName"));
-            dto.setCin(request.getParameter("cin"));
-            dto.setPhoneNumber(request.getParameter("telephone"));
-            dto.setWeight(request.getParameter("weight"));
-            dto.setBloodType(request.getParameter("bloodType"));
-            dto.setDateOfBirth(request.getParameter("dateDeNaissance"));
-            ReceiverDTO updatedReceiver = receiverService.updateReceiver(dto);
+
+            String receiverId = request.getParameter("id");
+
+            ReceiverDTO receiverDTO = receiverService.getReceiverById(Long.parseLong(receiverId));
+
+            if (null == receiverDTO) {
+                request.getSession().setAttribute(ERROR, "Receiver Not Found");
+                return CommandResult.redirect(request.getContextPath() + PATH);
+            }
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String telephone = request.getParameter("telephone");
+
+
+            receiverDTO.setFirstName(firstName);
+            receiverDTO.setLastName(lastName);
+            receiverDTO.setPhoneNumber(telephone);
+
+            ReceiverDTO updatedReceiver = receiverService.updateReceiver(receiverDTO);
+
             if(null == updatedReceiver) {
                 request.getSession().setAttribute(ERROR, "Error while updating receiver");
                 return CommandResult.redirect(request.getContextPath() + PATH);
