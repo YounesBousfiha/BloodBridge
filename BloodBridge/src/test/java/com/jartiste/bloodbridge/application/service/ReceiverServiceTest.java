20463c377ebcd0bce691f6d69ec9c25c6260f33e
Coderyounes <younesanas2021@gmail.com>
2025-10-15 16:03:24 +0100

    FEAT: Add unit tests for ReceiverService to validate receiver creation, update, deletion, and retrieval

---
+++ BloodBridge/src/test/java/com/jartiste/bloodbridge/application/service/ReceiverServiceTest.java
@@ -0,0 +1,124 @@
+package com.jartiste.bloodbridge.application.service;
+
+import com.jartiste.bloodbridge.application.mapper.ReceiverMapper;
+import com.jartiste.bloodbridge.domain.entity.Receiver;
+import com.jartiste.bloodbridge.domain.enums.BloodType;
+import com.jartiste.bloodbridge.domain.enums.StatusReceiver;
+import com.jartiste.bloodbridge.domain.enums.UrgentReceveur;
+import com.jartiste.bloodbridge.domain.repository.ReceiverRepository;
+import com.jartiste.bloodbridge.presentation.dto.ReceiverDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class ReceiverServiceTest {
+    private ReceiverDTO dto;
+
+    private Receiver receiver;
+
+
+    @Mock
+    private ReceiverRepository receiverRepository;
+
+    private ReceiverService receiverService;
+
+    @BeforeEach
+    void setUp(){
+
+        dto = new ReceiverDTO();
+        dto.setId(1L);
+        dto.setFirstName("John");
+        dto.setLastName("Doe");
+        dto.setCin("12345678");
+        dto.setBloodType(BloodType.O_PLUS);
+        dto.setPhoneNumber("06112233445");
+        dto.setUrgencyLevel(UrgentReceveur.CRITICAL);
+        dto.setStatusReceiver(StatusReceiver.EN_ATTENTE);
+        dto.setRequiredBags(4);
+        dto.setReceivedBags(0);
+
+
+        receiver = ReceiverMapper.toEntity(dto);
+        MockitoAnnotations.openMocks(this);
+        receiverService = new ReceiverService(receiverRepository);
+    }
+
+
+    @Test
+    void testCreateReceiver(){
+        when(receiverRepository.save(any(Receiver.class))).thenReturn(receiver);
+
+        dto = receiverService.createReceiver(dto);
+
+        assertEquals("John", dto.getFirstName());
+        assertEquals("Doe", dto.getLastName());
+        assertEquals("12345678", dto.getCin());
+        assertEquals(BloodType.O_PLUS, dto.getBloodType());
+        assertEquals("06112233445", dto.getPhoneNumber());
+        assertEquals(UrgentReceveur.CRITICAL, dto.getUrgencyLevel());
+        assertEquals(StatusReceiver.EN_ATTENTE, dto.getStatusReceiver());
+        assertEquals(4, dto.getRequiredBags());
+        assertEquals(0, dto.getReceivedBags());
+    }
+
+    @Test
+    void testUpdateReceiver(){
+
+        receiver.setFirstName("Hassan");
+
+        when(receiverRepository.save(any(Receiver.class))).thenReturn(receiver);
+
+        dto.setFirstName("Hassan");
+
+        dto = receiverService.updateReceiver(dto);
+
+        assertEquals("Hassan", dto.getFirstName());
+        assertEquals("Doe", dto.getLastName());
+        assertEquals("12345678", dto.getCin());
+        assertEquals(BloodType.O_PLUS, dto.getBloodType());
+        assertEquals("06112233445", dto.getPhoneNumber());
+        assertEquals(UrgentReceveur.CRITICAL, dto.getUrgencyLevel());
+        assertEquals(StatusReceiver.EN_ATTENTE, dto.getStatusReceiver());
+        assertEquals(4, dto.getRequiredBags());
+        assertEquals(0, dto.getReceivedBags());
+
+    }
+
+
+    @Test
+    void testDeleteReceiver(){
+        when(receiverRepository.isExist(1L)).thenReturn(true);
+
+        boolean result = receiverService.deleteReceiver(1L);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testGetAllReceivers(){
+        when(receiverRepository.findAll()).thenReturn(Optional.of(List.of(receiver)));
+
+        List<ReceiverDTO> result  = receiverService.getAllReceivers();
+
+        assertFalse(result.isEmpty());
+    }
+
+    @Test
+    void testGetReceiverById(){
+
+        when(receiverRepository.isExist(1L)).thenReturn(true);
+        when(receiverRepository.findById(1L)).thenReturn(Optional.of(receiver));
+
+        var result  = receiverService.getReceiverById(1L);
+
+        assertEquals("John", result.getFirstName());
+    }
+}
